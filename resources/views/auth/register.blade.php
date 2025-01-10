1b4571c0ca7b0a94d2995979cb978432f081a315
Ahmad Asmid <Ahmadnaza167@gmail.com>
2025-01-10 11:34:31 +0700

    mengganti dashboard

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -25,7 +25,7 @@
 
         <div class="mt-4">
             <x-input-label for="role" :value="__('Role')" />
-            <select id="role" name="role" class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" required>
+            <select id="role" name="role" class="block mt-1 w-full border-purple-300 focus:border-red-500 focus:ring-red-500 rounded-md shadow-sm" required>
                 <option value="" disabled selected>{{ __('Select Role') }}</option>
                 <option value="owner">{{ __('Owner') }}</option>
                 <option value="manager">{{ __('Manager') }}</option>
